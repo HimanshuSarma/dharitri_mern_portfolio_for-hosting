b94c928e855f3940c1da4982033711430e7f83ef
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-08 05:18:39 -0700

    commit

--- server.js
+++ server.js
@@ -103,21 +103,19 @@ app.post('/payment-verify', async(req, res) => {
 
     if (digest === req.headers['x-razorpay-signature']) {
 
-        console.log(req.body.payload.payment.entity.order_id, 'webook');
+        const user = await CustomerSchema.find({ 'orders.orderID': req.body.payload.payment.entity.order_id });
 
-        // const user = await CustomerSchema.find({ 'orders.orderID': req.body.payload.payment.entity.order_id });
+        for (let i = 0; i < user.orders.length; i++) {
+            if (user.orders[i].orderID === req.body.payload.payment.entity.order_id) {
+                user.orders[i].isPaid = true;
+                await user.save();
+                break;
+            } else if ((i === (user.orders.length - 1)) && user.orders[i].orderID !== req.body.payload.payment.entity.order_id) {
+                // Something is wrong. The orderId is not present in the user's orders array...
+                break;
+            }
+        }
 
-        // for (let i = 0; i < user.orders.length; i++) {
-        //     if (user.orders[i].orderID === req.body.payload.payment.entity.order_id) {
-        //         user.orders[i].isPaid = true;
-        //         await user.save();
-        //         break;
-        //     } else if ((i === (user.orders.length - 1)) && user.orders[i].orderID !== req.body.payload.payment.entity.order_id) {
-        //         // Something is wrong. The orderId is not present in the user's orders array...
-        //         break;
-        //     }
-        // }
-        // process it
         res.json({ status: 'ok' });
     } else {
         // pass it
